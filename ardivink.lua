78203e3466ee9f7c8a23d8f8a80837fba7d0f6b1
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2013-06-07 00:59:29 +0200

    Setup ardivink.
    Ignore-this: b059c9d3e37f879c7905e9c5f2c1f425
    
    darcs-hash:20130606225929-4d692-35807451b3a55dc356019dd16c505b3f65a35dda

---
+++ ardivink.lua
@@ -0,0 +1,17 @@
+oasis = require("oasis")
+darcs = require("darcs")
+ci = require("ci")
+dist = require("dist")
+
+ci.init()
+oasis.init()
+darcs.init()
+
+ci.prependenv("PATH", "/usr/opt/godi/bin")
+ci.prependenv("PATH", "/usr/opt/godi/sbin")
+ci.putenv("OUNIT_OUTPUT_HTML_DIR", dist.make_filename("ounit-log.html"))
+ci.putenv("OUNIT_OUTPUT_JUNIT_FILE", dist.make_filename("junit.xml"))
+ci.putenv("OUNIT_OUTPUT_FILE", dist.make_filename("ounit-log.txt"))
+
+oasis.std_process("--enable-tests")
+darcs.create_tag(oasis.package_version())
